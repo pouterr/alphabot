086d6db8af1bb4eed5a9b383ef71fccb0a19abbb
pouter <100417968+pouterr@users.noreply.github.com>
2022-10-07 13:47:09 +0600

    Update alphabotParser_bot.py

--- alphabotParser_bot.py
+++ alphabotParser_bot.py
@@ -3,9 +3,6 @@ from alphabotParser import main
 
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
     async def on_message(self,message):
         print('Message from {0.author}: {0.content}'.format(message))
 
