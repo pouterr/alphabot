67d814a23a13f4d46f2fb46083f6fe37e05d28a0
pouter <100417968+pouterr@users.noreply.github.com>
2022-10-07 13:40:39 +0600

    Add files via upload

---
+++ alphabotParser.py
@@ -0,0 +1,28 @@
+import fake_useragent, requests
+
+projects_list_ids = []
+print('program started')  
+user_agent = fake_useragent.UserAgent().random
+headers = {
+    "User-Agent": user_agent
+}
+url = "https://www.alphabot.app/api/projects?sort=startDate&scope=all&sortDir=-1&showHidden=true&pageSize=16&pageNum=0&search="
+def main():
+    if len(projects_list_ids) > 500:
+        projects_list_ids.clear()
+    req = requests.get(url, headers=headers)
+    projects = req.json()
+    for project in projects:
+        if project['_id'] not in projects_list_ids :
+            projects_list_ids.append(project["_id"]) 
+            name = project['name']
+            if project['discordUrl'] == '':
+                discord = 'Нет Сервера'
+            else:
+                discord = project['discordUrl']
+            res = f'----------------------------\nНазвание:{name}\nСервер:{discord}'
+            return res
+if __name__ == '__main__':
+    main() 
+    
+    
---
+++ alphabotParser_bot.py
@@ -0,0 +1,25 @@
+import discord, time
+from alphabotParser import main
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self,message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+    async def on_ready(self):
+        channel = self.get_channel(1027814569697087560)
+        while True:
+            try:
+                await channel.send(main())
+            except Exception:
+                print('нет новых проектов,ждем')
+                continue
+            time.sleep(1)
+
+
+
+client = MyClient()
+client.run('MTAyNzgxNDg0MTE0NDA0OTY3NA.GvMFLq.nqLOPhoCwnZTbH_XkPkdaRdiN0HY0vYS0QblaM')
